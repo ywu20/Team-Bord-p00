3da76710a770fbe13ed8a683778f22ceeeccf44e
Austin Ngan <angan20@stuy.edu>
2021-11-04 09:19:49 -0400

    added provisionary addBlog method that will use unique strings to be stored as info for the blog

--- app/userdb.py
+++ app/userdb.py
@@ -12,13 +12,14 @@ c.execute(command)
 
 command2 = '''CREATE TABLE IF NOT EXISTS blogs(
     username TEXT NOT NULL PRIMARY KEY UNIQUE,
-    blogKey TEXT NOT NULL)'''
+    blogKey TEXT NOT NULL
+    blogText TEXT NOT NULL)'''
 
 def addUser(username, password):
     c.execute("INSERT INTO logins VALUES(?, ?)", (username, password))
 
 
-def addBlog(username, blogKey):
+def addBlog(username, blogKey, text):
 	c.execute("INSERT INTO blogs VALUES(?, ?)", (username, blogKey)
 
 #addUser("hello", "123123a")
