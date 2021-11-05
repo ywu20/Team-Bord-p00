407221421ed5188b3abc0f8ed4fb21fee66cf7a3
Austin Ngan <angan20@stuy.edu>
2021-11-05 09:14:13 -0400

    fixed error where function did not add to db

--- app/userdb.py
+++ app/userdb.py
@@ -12,18 +12,26 @@ c.execute(command)
 
 command2 = '''CREATE TABLE IF NOT EXISTS blogs(
     username TEXT NOT NULL PRIMARY KEY UNIQUE,
-    blogKey TEXT NOT NULL
+    blogKey TEXT NOT NULL,
     blogText TEXT NOT NULL)'''
+c.execute(command2)
 
 def addUser(username, password):
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
     c.execute("INSERT INTO logins VALUES(?, ?)", (username, password))
+    db.commit()
+    db.close()
 
 
 def addBlog(username, blogKey, text):
-    c.execute("INSERT INTO blogs VALUES(?, ?)", (username, blogKey)
+    db = sqlite3.connect(DB_FILE)
+    c = db.cursor()
+    c.execute("INSERT INTO blogs VALUES(?, ?)", (username, blogKey))
+    db.commit()
+    db.close()
 
-#addUser("hello", "123123a")
 
-db.commit()
-db.close()
+# addUser("hello", "123123a")
+
 
